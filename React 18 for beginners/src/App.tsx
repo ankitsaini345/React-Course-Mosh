3c1baf00cc3a133b51cf8ad9bdb6d2efe2dc0d31
Ankit Saini <ankit-a.saini@db.com>
2023-09-20 19:51:46 +0530

    Expandable component added

--- React 18 for beginners/src/App.tsx
+++ React 18 for beginners/src/App.tsx
@@ -1,11 +1,12 @@
-import Alert from './components/Alert';
-import Button from './components/Button/Button';
-import Like from './components/Like';
-import ListGroup from './components/ListGroup/ListGroup';
-import Message from './components/Message'
+import Alert from "./components/Alert";
+import Button from "./components/Button/Button";
+import ExpendableText from "./components/ExpendableText";
+import Like from "./components/Like";
+import ListGroup from "./components/ListGroup/ListGroup";
+import Message from "./components/Message";
+import MutateArray from "./Examples/MutateArray";
 
 function App() {
-
   // ----------------------for list group component
   // const items: string[] | null = ["A", "B", "C", "D"];
   // const handleSelect = (item: string) => {
@@ -21,8 +22,16 @@ function App() {
   // return <Button color='primary' childern = 'My Button' onClick={()=> console.log('Clicked')
   // }/>
 
-  return <Like onClick={()=> console.log('Clicked')
-  }/>
+  // return <Like onClick={()=> console.log('Clicked')
+  // }/>
+
+  return (
+    <ExpendableText maxChars={10}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+      exercitationem dolorem, labore voluptates voluptatibus corrupti doloremque
+      saepe impedit odit quasi.
+    </ExpendableText>
+  );
 }
 
 export default App;
