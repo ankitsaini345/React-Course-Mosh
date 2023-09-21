82d4c23d7e47860613583a9ea7828c5c3ec260c6
Ankit Saini <ankit-a.saini@db.com>
2023-09-21 12:02:50 +0530

    Simple form using useRef

--- React 18 for beginners/src/App.tsx
+++ React 18 for beginners/src/App.tsx
@@ -1,12 +1,15 @@
 import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
 import ExpendableText from "./components/ExpendableText";
+import Form from "./components/Form/Form";
 import Like from "./components/Like";
 import ListGroup from "./components/ListGroup/ListGroup";
 import Message from "./components/Message";
 import MutateArray from "./Examples/MutateArray";
 
 function App() {
+  return <Form />;
+
   // ----------------------for list group component
   // const items: string[] | null = ["A", "B", "C", "D"];
   // const handleSelect = (item: string) => {
@@ -25,13 +28,13 @@ function App() {
   // return <Like onClick={()=> console.log('Clicked')
   // }/>
 
-  return (
-    <ExpendableText maxChars={10}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
-      exercitationem dolorem, labore voluptates voluptatibus corrupti doloremque
-      saepe impedit odit quasi.
-    </ExpendableText>
-  );
+  // return (
+  //   <ExpendableText maxChars={10}>
+  //     Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+  //     exercitationem dolorem, labore voluptates voluptatibus corrupti doloremque
+  //     saepe impedit odit quasi.
+  //   </ExpendableText>
+  // );
 }
 
 export default App;
