28e0c1d44efd4df37bfe34b59c54568f36d21008
Ankit Saini <ankit-a.saini@db.com>
2023-09-23 19:29:06 +0530

    dark theme added

--- React 18 for beginners/src/Video Game App/VideoGameMain.tsx
+++ React 18 for beginners/src/Video Game App/VideoGameMain.tsx
@@ -1,10 +1,12 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Test from "./Test";
+import theme from "./theme";
 import VideoGameApp from "./VideoGameApp";
 
 const VideoGameMain = () => {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <VideoGameApp />
     </ChakraProvider>
   );
