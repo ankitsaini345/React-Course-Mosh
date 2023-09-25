f529dc34f77267261e89453cc281ea0183b5c3e6
Ankit Saini <ankitsaini345@gmail.com>
2023-09-25 16:57:18 +0530

    GenreList added

--- React 18 for beginners/src/Video Game App/components/GenreList.tsx
+++ React 18 for beginners/src/Video Game App/components/GenreList.tsx
@@ -1,14 +1,28 @@
+import { HStack, List, ListItem, Image, Text, Spinner } from "@chakra-ui/react";
 import React from "react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-urls";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
