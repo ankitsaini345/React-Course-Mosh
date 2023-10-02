d90999ecc9c185c677278048da58609a6f24a993
Ankit Saini <ankitsaini345@gmail.com>
2023-10-02 16:24:18 +0530

    Refactor: code cleanup

--- React Intermidiate Topics/src/Starter Project Code/StarterProjectApp.tsx
+++ React Intermidiate Topics/src/Starter Project Code/StarterProjectApp.tsx
@@ -1,4 +1,3 @@
-import AuthProvider from "./state-management/AuthProvider";
 import Counter from "./state-management/counter/Counter";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
@@ -6,13 +5,11 @@ import { TaskProvider } from "./state-management/tasks";
 
 const StarterProjectApp = () => {
   return (
-    <AuthProvider>
-      <TaskProvider>
-        <Counter />
-        <NavBar />
-        <HomePage />
-      </TaskProvider>
-    </AuthProvider>
+    <TaskProvider>
+      <Counter />
+      <NavBar />
+      <HomePage />
+    </TaskProvider>
   );
 };
 
