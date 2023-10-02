4ab841727eb89b59cc6ad55b07c03289ed640e1b
Ankit Saini <ankitsaini345@gmail.com>
2023-10-02 15:21:27 +0530

    Custom Provider for tasks

--- React Intermidiate Topics/src/Starter Project Code/StarterProjectApp.tsx
+++ React Intermidiate Topics/src/Starter Project Code/StarterProjectApp.tsx
@@ -1,21 +1,15 @@
-import { useReducer } from "react";
 import AuthProvider from "./state-management/AuthProvider";
-import AuthContext from "./state-management/context/authContext";
-import TaskContext from "./state-management/context/tasksContext";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import authReducer from "./state-management/reducers/authReducer";
-import taskReducer from "./state-management/reducers/taskReducer";
+import TaskProvider from "./state-management/TaskProvider";
 
 const StarterProjectApp = () => {
-  const [tasks, dispatch] = useReducer(taskReducer, []);
-
   return (
     <AuthProvider>
-      <TaskContext.Provider value={{ tasks, dispatch }}>
+      <TaskProvider>
         <NavBar />
         <HomePage />
-      </TaskContext.Provider>
+      </TaskProvider>
     </AuthProvider>
   );
 };
