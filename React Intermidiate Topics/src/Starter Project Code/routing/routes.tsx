fd09bc1fc983e861941f004ccabaceca51111347
Ankit Saini <ankitsaini345@gmail.com>
2023-10-04 17:40:53 +0530

    Handling Error | Routing

--- React Intermidiate Topics/src/Starter Project Code/routing/routes.tsx
+++ React Intermidiate Topics/src/Starter Project Code/routing/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 import HomePage from "./HomePage";
 import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
@@ -8,6 +9,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "", element: <HomePage /> },
       {
